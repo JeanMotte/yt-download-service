106c6aec8307f3f55beb0a9807093e9f9994bd53
Jean Motte <jean.motte@polynom.io>
2025-07-19 17:53:50 +0000

    UPDATE: create user and history table migration

--- alembic/versions/90902160bb77_create_user_and_history_tables.py
+++ alembic/versions/90902160bb77_create_user_and_history_tables.py
@@ -6,6 +6,10 @@ Create Date: 2025-07-19 17:42:21.399073
 
 """
 
+import sqlalchemy as sa
+
+from alembic import op
+
 # revision identifiers, used by Alembic.
 revision = "90902160bb77"
 down_revision = None
@@ -15,9 +19,49 @@ depends_on = None
 
 def upgrade():
     """Create user and history tables."""
-    pass
+    op.create_table(
+        "user",
+        sa.Column("id", sa.dialects.postgresql.UUID(as_uuid=True), primary_key=True),
+        sa.Column("first_name", sa.String(), nullable=False),
+        sa.Column("last_name", sa.String(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False, unique=True),
+        sa.Column(
+            "created_at", sa.DateTime(), nullable=False, server_default=sa.func.now()
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime(),
+            nullable=False,
+            server_default=sa.func.now(),
+            onupdate=sa.func.now(),
+        ),
+    )
+    op.create_table(
+        "history",
+        sa.Column("id", sa.dialects.postgresql.UUID(as_uuid=True), primary_key=True),
+        sa.Column(
+            "user_id",
+            sa.dialects.postgresql.UUID(as_uuid=True),
+            sa.ForeignKey("user.id"),
+            nullable=False,
+        ),
+        sa.Column("yt_video_id", sa.String(), nullable=False),
+        sa.Column("start_time", sa.Integer(), nullable=False),
+        sa.Column("end_time", sa.Integer(), nullable=False),
+        sa.Column(
+            "created_at", sa.DateTime(), nullable=False, server_default=sa.func.now()
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime(),
+            nullable=False,
+            server_default=sa.func.now(),
+            onupdate=sa.func.now(),
+        ),
+    )
 
 
 def downgrade():
     """Drop user and history tables."""
-    pass
+    op.drop_table("history")
+    op.drop_table("user")
