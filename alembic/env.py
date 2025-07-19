6a54c5d6f7688f7f69e0f0f409a8b1d31a6de4dd
Jean Motte <jean.motte@polynom.io>
2025-07-19 17:46:45 +0000

    UPDATE: migration and start implementing tables

---
+++ alembic/env.py
@@ -0,0 +1,47 @@
+from logging.config import fileConfig
+
+from sqlalchemy import create_engine
+from yt_download_service.app.utils.env import get_or_raise_env
+
+from alembic import context
+
+config = context.config
+
+if config.config_file_name:
+    fileConfig(config.config_file_name)
+
+# Use base.metadata to autogenerate migration.
+target_metadata = None
+
+DB_NAME = get_or_raise_env("DB_NAME")
+DB_HOST = get_or_raise_env("DB_HOST")
+DB_PORT = get_or_raise_env("DB_PORT")
+DB_USER = get_or_raise_env("DB_USER")
+DB_PASS = get_or_raise_env("DB_PASS")
+DB_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+context.configure(
+    url=DB_URL,
+    target_metadata=target_metadata,
+    dialect_opts={"paramstyle": "named"},
+    compare_type=True,
+)
+
+
+def run_migrations() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = create_engine(DB_URL)
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+run_migrations()
---
+++ alembic/versions/90902160bb77_create_user_and_history_tables.py
@@ -0,0 +1,23 @@
+"""Create user and history tables.
+
+Revision ID: 90902160bb77
+Revises:
+Create Date: 2025-07-19 17:42:21.399073
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "90902160bb77"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """Create user and history tables."""
+    pass
+
+
+def downgrade():
+    """Drop user and history tables."""
+    pass
